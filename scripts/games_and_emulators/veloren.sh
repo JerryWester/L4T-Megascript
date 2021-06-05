c7a9792902b91d4cdd121e05f542d5665e911547
Jerry Wester <JerryWester@users.noreply.github.com>
2021-06-05 14:19:57 -0600

    Create veloren.sh

---
+++ scripts/games_and_emulators/veloren.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "This process will install Veloren, a multiplayer voxel RPG written in Rust."
+echo "It is inspired by games such as Cube World, Legend of Zelda: Breath of the Wild,"
+echo "Dwarf Fortress and Minecraft."
+
+sudo apt install -y wget git
+
+echo "Installing Rust..."
+cd ~
+wget -O rustup-init.sh https://sh.rustup.rs
+chmod +x rustup-init.sh
+./rustup-init.sh -qy
+source $HOME/.cargo/env
+
+echo "Cloning Veloren..."
+git clone https://gitlab.com/veloren/veloren.git
+cd veloren
+echo "Building Veloren. This might take a while..."
+cargo build
